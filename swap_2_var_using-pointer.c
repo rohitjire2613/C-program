f6586d826dd156e9414e548c7c53c1eb1f072a13
rohitjire2613 <jirerohit02@gmail.com>
2026-02-26 18:37:50 +0530

    Create swap_2_var_using-pointer.c

---
+++ swap_2_var_using-pointer.c
@@ -0,0 +1,24 @@
+/*Write a function to swap value of two variables using pointer*/
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 10, y = 20;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swap:  x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+
+
+**Output:**
+```
+Before swap: x = 10, y = 20
+After swap:  x = 20, y = 10
